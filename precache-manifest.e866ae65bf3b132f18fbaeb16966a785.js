4eab0d0bb90340cbd9b34e6fa49bfdb35e9c4103
vadim <vadim_lyutsko@mail.ru>
2022-11-26 10:04:04 +0300

    Updates

---
+++ precache-manifest.e866ae65bf3b132f18fbaeb16966a785.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf1fd495f4de411d23e003d5f7e5b41",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "4bc943f41f2dafb8970b",
+    "url": "/Todolist/static/css/main.9cb3b14e.chunk.css"
+  },
+  {
+    "revision": "803a0950a954674fc63f",
+    "url": "/Todolist/static/js/2.c3663e00.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/Todolist/static/js/2.c3663e00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bc943f41f2dafb8970b",
+    "url": "/Todolist/static/js/main.50469c7c.chunk.js"
+  },
+  {
+    "revision": "8869effc02c0632c6b5e",
+    "url": "/Todolist/static/js/runtime-main.2e1fef4f.js"
+  }
+]);
